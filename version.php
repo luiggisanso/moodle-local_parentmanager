5d5052686b3401a0c643fdf0582a21b39115e9a7
Luiggi Sansonetti <1565841+luiggisanso@users.noreply.github.com>
2026-02-28 11:28:48 +0100

    Update version.php

--- version.php
+++ version.php
@@ -27,8 +27,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_parentmanager';
-$plugin->version   = 2026020900; // Date YYYYMMDDxx
+$plugin->version   = 2026022800; // Date YYYYMMDDxx
 $plugin->requires  = 2022112800; // Moodle 4.1+
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0';
 $plugin->supported = [401, 501];
+
